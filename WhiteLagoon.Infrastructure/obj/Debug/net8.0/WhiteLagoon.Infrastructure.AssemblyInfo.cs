4eb08d71dc7f380200676422ed7e535e6e0d8654
khoi nguyen <nguyen123mitu@gmail.com>
2025-02-12 16:05:22 +0700

    feat: scpoce css

--- WhiteLagoon.Infrastructure/obj/Debug/net8.0/WhiteLagoon.Infrastructure.AssemblyInfo.cs
+++ WhiteLagoon.Infrastructure/obj/Debug/net8.0/WhiteLagoon.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhiteLagoon.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631672d7adf945f757717ce0ac6a1993c08e789b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830233279641d7f31d753b3df0052b888f8fcbfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhiteLagoon.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhiteLagoon.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
